40dded96dfd0dd14a17368546e6e4ab696b63fc6
Baani Pasrija <bp2195@nyu.edu>
2023-11-25 13:41:35 -0500

    time & tempo code

--- machine-learning-client/ml.py
+++ machine-learning-client/ml.py
@@ -9,6 +9,7 @@ import numpy as np
 import subprocess
 import os
 import logging
+import librosa
 
 app = Flask(__name__)
 
@@ -29,6 +30,8 @@ def process_data():
     try:
         webm_file = 'temp_recording.webm'
         wav_file = 'temp_recording.wav'
+        midi_file = 'output.mid'
+
         with open(webm_file, 'wb') as file:
             file.write(request.files['audio'].read())
         result = subprocess.run(['ffmpeg', '-i', webm_file, wav_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -56,14 +59,51 @@ def process_data():
         notes_data_sorted = sorted(notes_data, key=lambda x: x['time'])
         #  logging.info(f"Chunked notes data for jsonify: {notes_data_sorted}")
 
+        onsets = detect_note_onsets(wav_file)
+        durations = estimate_note_durations(onsets, audio_length=len(audio) / sr)
+        tempo = estimate_tempo(wav_file)
+
+        create_midi(notes_data_sorted, onsets, durations, tempo, output_file=midi_file)
+
         os.remove(webm_file)
         os.remove(wav_file)
 
         return jsonify(notes_data_sorted)
+        #store file in database, grab from there and show.
         
     except Exception as e:
         app.logger.error(f"Error processing data: {e}")
         return jsonify({"error": str(e)}), 500
+
+def detect_note_onsets(audio_file):
+    y, sr = librosa.load(audio_file, sr=None)
+    onsets = librosa.onset.onset_detect(y=y, sr=sr, units='time')
+    return onsets
+
+def estimate_note_durations(onsets, audio_length):
+    durations = np.diff(onsets, append=audio_length)
+    return durations
+
+def estimate_tempo(audio_file):
+    y, sr = librosa.load(audio_file, sr=None)
+    tempo, _ = librosa.beat.beat_track(y, sr=sr)
+    return tempo
+
+def create_midi(pitch_data, onsets, durations, tempo, output_file='output.mid'):
+    midi_data = pretty_midi.PrettyMIDI()
+    midi_data.estimate_tempo = tempo
+    instrument = pretty_midi.Instrument(program=pretty_midi.instrument_name_to_program('Acoustic Grand Piano'))
+
+    for pitch, onset, duration in zip(pitch_data, onsets, durations):
+        note_number = pretty_midi.note_name_to_number(pitch['note'])
+        start_time = onset
+        end_time = start_time + duration
+
+        note = pretty_midi.Note(velocity=100, pitch=note_number, start=start_time, end=end_time)
+        instrument.notes.append(note)
+
+    midi_data.instruments.append(instrument)
+    midi_data.write(output_file)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
