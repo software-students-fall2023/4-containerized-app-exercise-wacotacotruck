c029f4df244847230cf90c7dee1f44e5bd87fd39
awesomeadi00 <adityapandhare44@gmail.com>
2023-11-24 17:44:46 -0800

    Added pep8 structure to python files

--- machine-learning-client/ml.py
+++ machine-learning-client/ml.py
@@ -1,14 +1,12 @@
 """Module for the machine learning client."""
+import os
+import subprocess
+import logging
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import io 
 import crepe
 import pretty_midi
 import soundfile as sf
-import numpy as np
-import subprocess
-import os
-import logging
 
 app = Flask(__name__)
 
@@ -26,43 +24,44 @@ def frequency_to_note_name(frequency):
 
 @app.route("/process", methods=["POST"])
 def process_data():
+    """Route to process the data."""
     try:
         webm_file = 'temp_recording.webm'
         wav_file = 'temp_recording.wav'
         with open(webm_file, 'wb') as file:
             file.write(request.files['audio'].read())
-        result = subprocess.run(['ffmpeg', '-i', webm_file, wav_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(['ffmpeg', '-i', webm_file, wav_file], 
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
         if result.returncode != 0:
             print("ffmpeg error:", result.stderr.decode())
-            raise Exception("Error converting WebM to WAV")
-        
-        audio, sr = sf.read(wav_file)
+            raise ValueError("Error converting WebM to WAV")
+        audio, sr = sf.read(wav_file)        
         # logging.info(f"Audio data type: {audio.dtype}, Sample rate: {sr}")
-        
-        confidence_threshold = 0.74 
-        chunk_size = 1024 * 10  
+        confidence_threshold = 0.74
+        chunk_size = 1024 * 10
         notes_data = []
 
         for start in range(0, len(audio), chunk_size):
             end = start + chunk_size
             audio_chunk = audio[start:end]
             time, frequency, confidence, activation = crepe.predict(audio_chunk, sr, viterbi=True)
-            
-            for t, f, c in zip(time, frequency, confidence):
+
+            for t, f, c in zip(time, frequency, confidence, activation):
                 if c >= confidence_threshold:
                     note_name = frequency_to_note_name(f)
-                    notes_data.append({"time": float(t), "note": note_name, "confidence": round(float(c), 2) })
+                    notes_data.append({"time": float(t), "note": note_name, 
+                                        "confidence": round(float(c), 2)})
 
         notes_data_sorted = sorted(notes_data, key=lambda x: x['time'])
-        #  logging.info(f"Chunked notes data for jsonify: {notes_data_sorted}")
+        # logging.info(f"Chunked notes data for jsonify: {notes_data_sorted}")
 
         os.remove(webm_file)
         os.remove(wav_file)
 
         return jsonify(notes_data_sorted)
-        
-    except Exception as e:
-        app.logger.error(f"Error processing data: {e}")
+
+    except ValueError as e:
+        app.logger.error("Error processing data: %s", e)
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
--- web-app/app.py
+++ web-app/app.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 
-# Initializes flask application and loads the .env file from the MongoDB Atlas Database
+# Initializes Flask application and loads the .env file from the MongoDB Atlas Database
 app = Flask(__name__)
 load_dotenv()
 
@@ -28,28 +28,28 @@ try:
     database = client[os.getenv('MONGO_DBNAME')]
     print('* Connected to MongoDB!')
 
-except Exception as err:
+except ConnectionError as err:
     print('* "Failed to connect to MongoDB at', os.getenv('MONGO_URI'))
     print('Database connection error:', err)
 
 # Routes
 @app.route("/")
 def index():
-    """Description of what the function does."""
+    """Renders the home page"""
     return render_template('index.html')
 
 def call_ml_client(data):
-    """Description of what the function does."""
+    """Contacts the ml client"""
     response = requests.post("http://localhost:5002/process", json=data, timeout=10)
     return response.json()
 
 # This is the function which registers the signup page from the login.html page if the user does click it
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    """Renders the signup page"""
     # If there is a user_id in session, it redirects them back to the home page
     if 'user_id' in session:
         return redirect(url_for('index'))
-    
     # If there is no account, then we allow the user to creat one.
     else:
         if request.method == 'POST':
@@ -59,21 +59,20 @@ def signup():
             password = request.form['password']
             confirm_password = request.form['confirm_password']
             email = request.form['email']
-
             errors = []
-            
+
             # This checks if there is already a user that has this exact username
             if database.users.find_one({'username': username}):
                 errors.append("Username already exists!")
-            
+
             # This checks if there is already a user that has this exact email
             if database.users.find_one({'email': email}):
                 errors.append("Email already used, try another or try logging in!")
-            
+
             # This checks if the password is in between 8-20 characters
-            if len(password) < 8 & len(password) > 20:
+            if not 8 <= len(password) <= 20:
                 errors.append("Password must be between 8 and 20 characters long!")
-            
+
             # This checks if the password does not have any numbers
             if not any(char.isdigit() for char in password):
                 errors.append("Password should have at least one number!")
@@ -81,16 +80,16 @@ def signup():
             # This checks if the password does not have any alphabets
             if not any(char.isalpha() for char in password):
                 errors.append("Password should have at least one alphabet!")
-            
+
             # This checks if the password and the confirm password do not match
             if not confirm_password == password:
                 errors.append("Passwords do not match!")
 
-            # If there are any errors, it will re-render the signup.html page and allow the user to try again
+            # If any errors, it will re-render the signup.html page and allow the user to try again
             if errors:
                 return render_template('signup.html', errors=errors)
-            
-            # If the user managed to create a proper account, it will generate a hash for their password 
+
+            # If user managed to create a proper account, it will generate a hash for their password
             else:
                 password_hash = generate_password_hash(password)
 
@@ -103,28 +102,30 @@ def signup():
                 'midi_files': []
             })
 
-            # Once that's done, it will redirect the user to the login page where they must login to access the webpage. 
+            # This redirects the user to the login page where they must login to access the webpage.
             return redirect(url_for('login'))
-        
+ 
         # Renders the signup.html page
         return render_template('signup.html')
 
 # This function is for rendering either the login.html template if they haven't logged in and if they have, then we render the home page
 @app.route('/login', methods=['GET'])
-def login(): 
+def login():
+    """Renders the login page"""
     if 'user_id' in session:
         return redirect(url_for('index'))
-    
+
     else:
         return render_template('login.html')
 
 # This function is the backend of the login functionality from the login.html file
 @app.route('/login_auth', methods=['POST'])
-def login_auth(): 
+def login_auth():
+    """Route for login authentication"""
     # If a user is already logged in, redirect them to the home page
     if 'user_id' in session:
         return redirect(url_for('index'))
-    
+
     # Else, ask them to login by inputting a username and password
     else:
         if request.method == 'POST':
@@ -134,69 +135,47 @@ def login_auth():
 
             errors = []
 
-             # Once they've inputted their username and password, we search the database if there is someone already with this username
+            # Once inputted their username and password, check the database for existing users
             user = database.users.find_one({'username': username})
 
-            # If we have a user and the password they gave matches with the hash function, we provide the _id attribute of the user to the user_id in the session
+            # We provide the _id attribute of the user to the user_id in the session
             if user and check_password_hash(user['password'], password):    
-                # This is how keep keep track of which user is currently authenticated or logged in. We store the user_id string into the user's session
+                # User sessions to keep track of who's logged in
                 session['user_id'] = str(user['_id'])
                 return redirect(url_for('index'))
-            
-            # If the username or password does not match, that means either one is wrong, hence we render the login.html template so they can attempt once more.
+
+            # If the username or password does not match we render the login.html template once more
             else:
                 errors.append("Invalid username or password!")
                 return render_template('login.html', errors=errors)
-            
-@app.route('/forgot_password', methods = ['GET','POST'])
+
+@app.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
+    """Renders the forgot password page"""
     if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            confirm_password = request.form['confirm_password']
-            email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        email = request.form['email']
 
-            errors = []
-            
-            user = database.users.find_one({'email': email, 'username':username})
+        errors = []
 
-            if not user:
-                errors.append("Invalid username or email!")
-            
-            if len(password) < 8 & len(password) > 20:
-                errors.append("Password must be between 8 and 20 characters long!")
-            
-            if not any(char.isdigit() for char in password):
-                errors.append("Password should have at least one number!")
-            
-            if not any(char.isalpha() for char in password):
-                errors.append("Password should have at least one alphabet!")
-            
-            if not confirm_password == password:
-                errors.append("Passwords do not match!")
+        user = database.users.find_one({'email': email, 'username':username})
 
-            if errors:
-                return render_template('forgot_password.html', errors=errors)
-            
-            else:
-                password_hash = generate_password_hash(password)
+        if not user:
+            errors.append("Invalid username or email!")
 
-                filter = {'email': email, 'username':username}
-                            
-                update = {'$set': {'password': password_hash}}
+        if not 8 <= len(password) <= 20:
+            errors.append("Password must be between 8 and 20 characters long!")
 
-                database.users.update_one(filter, update)
-                                     
-                return redirect(url_for('login'))
-            
-    return render_template('forgot_password.html')
+        if not any(char.isdigit() for char in password):
+            errors.append("Password should have at least one number!")
 
-# Here we have another route, if the user decides to logout, it will pop their user_id from the session and redirect them to the login page
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('login'))
+        if not any(char.isalpha() for char in password):
+            errors.append("Password should have at least one alphabet!")
 
-# Executing the Flask Application: 
-if(__name__ == "__main__"):
-    app.run(debug=True)
+        if not confirm_password == password:
+            errors.append("Passwords do not match!")
+
+        if errors:
+            return render_template('forgot_password.html', errors=errors)
