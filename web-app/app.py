9a1a973599cf8c1bddbbaf13b7fa2aad5e25df96
awesomeadi00 <adityapandhare44@gmail.com>
2023-11-24 18:50:29 -0800

    Added some more PEP8 to app.py

--- web-app/app.py
+++ web-app/app.py
@@ -50,63 +50,62 @@ def signup():
     # If there is a user_id in session, it redirects them back to the home page
     if 'user_id' in session:
         return redirect(url_for('index'))
+
     # If there is no account, then we allow the user to creat one.
-    else:
-        if request.method == 'POST':
+    if request.method == 'POST':
 
-            # We allow the user to create their username, password, confirm password, email
-            username = request.form['username']
-            password = request.form['password']
-            confirm_password = request.form['confirm_password']
-            email = request.form['email']
-            errors = []
+        # We allow the user to create their username, password, confirm password, email
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        email = request.form['email']
+        errors = []
 
-            # This checks if there is already a user that has this exact username
-            if database.users.find_one({'username': username}):
-                errors.append("Username already exists!")
+        # This checks if there is already a user that has this exact username
+        if database.users.find_one({'username': username}):
+            errors.append("Username already exists!")
 
-            # This checks if there is already a user that has this exact email
-            if database.users.find_one({'email': email}):
-                errors.append("Email already used, try another or try logging in!")
+        # This checks if there is already a user that has this exact email
+        if database.users.find_one({'email': email}):
+            errors.append("Email already used, try another or try logging in!")
 
-            # This checks if the password is in between 8-20 characters
-            if not 8 <= len(password) <= 20:
-                errors.append("Password must be between 8 and 20 characters long!")
+        # This checks if the password is in between 8-20 characters
+        if not 8 <= len(password) <= 20:
+            errors.append("Password must be between 8 and 20 characters long!")
 
-            # This checks if the password does not have any numbers
-            if not any(char.isdigit() for char in password):
-                errors.append("Password should have at least one number!")
+        # This checks if the password does not have any numbers
+        if not any(char.isdigit() for char in password):
+            errors.append("Password should have at least one number!")
 
-            # This checks if the password does not have any alphabets
-            if not any(char.isalpha() for char in password):
-                errors.append("Password should have at least one alphabet!")
+        # This checks if the password does not have any alphabets
+        if not any(char.isalpha() for char in password):
+            errors.append("Password should have at least one alphabet!")
 
-            # This checks if the password and the confirm password do not match
-            if not confirm_password == password:
-                errors.append("Passwords do not match!")
+        # This checks if the password and the confirm password do not match
+        if not confirm_password == password:
+            errors.append("Passwords do not match!")
 
-            # If any errors, it will re-render the signup.html page and allow the user to try again
-            if errors:
-                return render_template('signup.html', errors=errors)
+        # If any errors, it will re-render the signup.html page and allow the user to try again
+        if errors:
+            return render_template('signup.html', errors=errors)
 
-            # If user managed to create a proper account, it will generate a hash for their password
-            else:
-                password_hash = generate_password_hash(password)
+        # If user managed to create a proper account, it will generate a hash for their password
+        password_hash = generate_password_hash(password)
 
-            # Here we insert their account details to the database
-            collection = database['users']
-            collection.insert_one({
-                'username': username,
-                'password': password_hash,
-                'email': email,
-                'midi_files': []
-            })
+        # Here we insert their account details to the database
+        collection = database['users']
+        collection.insert_one({
+            'username': username,
+            'password': password_hash,
+            'email': email,
+            'midi_files': []
+        })
 
-            # This redirects the user to the login page where they must login to access the webpage.
-            return redirect(url_for('login'))
+        # This redirects the user to the login page where they must login to access the webpage.
+        return redirect(url_for('login'))
 
-        # Renders the signup.html page
-        return render_template('signup.html')
+    # Renders the signup.html page
+    return render_template('signup.html')
 
 # Rendering either the login template if they haven't logged in, otherwise, the home page
 @app.route('/login', methods=['GET'])
@@ -127,27 +126,26 @@ def login_auth():
         return redirect(url_for('index'))
 
     # Else, ask them to login by inputting a username and password
-    else:
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            session['username'] = username
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
 
-            errors = []
+        errors = []
 
-            # Once inputted their username and password, check the database for existing users
-            user = database.users.find_one({'username': username})
+        # Once inputted their username and password, check the database for existing users
+        user = database.users.find_one({'username': username})
 
-            # We provide the _id attribute of the user to the user_id in the session
-            if user and check_password_hash(user['password'], password):
-                # User sessions to keep track of who's logged in
-                session['user_id'] = str(user['_id'])
-                return redirect(url_for('index'))
+        # We provide the _id attribute of the user to the user_id in the session
+        if user and check_password_hash(user['password'], password):
+            # User sessions to keep track of who's logged in
+            session['user_id'] = str(user['_id'])
+            return redirect(url_for('index'))
 
-            # If the username or password does not match we render the login.html template once more
-            else:
-                errors.append("Invalid username or password!")
-                return render_template('login.html', errors=errors)
+        # If the username or password does not match we render the login.html template once more
+        errors.append("Invalid username or password!")
+        return render_template('login.html', errors=errors)
+    return None
 
 @app.route('/forgot_password', methods=['GET', 'POST'])
 def forgot_password():
@@ -157,9 +155,7 @@ def forgot_password():
         password = request.form['password']
         confirm_password = request.form['confirm_password']
         email = request.form['email']
-
         errors = []
-
         user = database.users.find_one({'email': email, 'username':username})
 
         if not user:
@@ -179,3 +175,4 @@ def forgot_password():
 
         if errors:
             return render_template('forgot_password.html', errors=errors)
+    return None
