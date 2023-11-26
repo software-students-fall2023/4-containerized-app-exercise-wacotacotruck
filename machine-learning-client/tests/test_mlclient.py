820adbed23d77c338a9b79662d75bd77249a4c93
awesomeadi00 <adityapandhare44@gmail.com>
2023-11-25 22:04:01 -0800

    Added some setups for pytesting folders

---
+++ machine-learning-client/tests/test_mlclient.py
@@ -0,0 +1,17 @@
+"""Module for Testing Python Functions"""
+import pytest
+
+class Tests:
+    """Test Functions for the Machine Learning Client"""
+
+    @pytest.fixture
+    def example_fixture(self):
+        """An example of a pytest fixture"""
+        yield
+
+    # Test functions
+    def test_sanity_check(self):
+        """Run a simple test that always passes."""
+        expected = True
+        actual = True
+        assert actual == expected, "Expected True to be equal to True!"
