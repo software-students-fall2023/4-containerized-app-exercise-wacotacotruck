cd8a765a1beaa9df3df8c7261112fb05c113d63c
ccczy-czy <zc2122@nyu.edu>
2023-12-02 02:08:56 -0500

    add test functions for ml.py

--- machine_learning_client/tests/test_mlclient.py
+++ machine_learning_client/tests/test_mlclient.py
@@ -1,11 +1,12 @@
 """Module for Testing Python Functions"""
 import random
-import pytest
 import os
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
+import subprocess
+import pytest
 import numpy as np
 import pretty_midi
-from .. import ml
+from machine_learning_client import ml
 
 
 class Tests:
@@ -152,7 +153,266 @@ class Tests:
         ]
         assert actual in midi_note_names
 
-    # This function is producing 7 warnings about Deprecation, not sure how to remove these...
+    # Test for successful convert_webm_to_wav
+    @patch("machine_learning_client.ml.subprocess.run")
+    def test_convert_webm_to_wav_success(self, mock_run):
+        """Given WebM audio file, convert to WAV format."""
+        mock_run.return_value.returncode = 0
+
+        ml.convert_webm_to_wav("test_input.webm", "test_output.wav")
+
+        mock_run.assert_called_with(
+            ["ffmpeg", "-i", "test_input.webm", "test_output.wav"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=False,
+        )
+
+    # Test for failure convert_webm_to_wav
+    @patch("machine_learning_client.ml.subprocess.run")
+    def test_convert_webm_to_wav_failure(self, mocker):
+        """Test failure scenario for convert_webm_to_wav."""
+        mocker.return_value = MagicMock(returncode=1, stderr=MagicMock())
+        mocker.return_value.stderr.decode.return_value = "ffmpeg error message"
+
+        with pytest.raises(ValueError) as exc_info:
+            ml.convert_webm_to_wav("test_input.webm", "test_output.wav")
+
+        assert "Error converting WebM to WAV" in str(exc_info.value)
+
+        mocker.assert_called_with(
+            ["ffmpeg", "-i", "test_input.webm", "test_output.wav"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=False,
+        )
+
+    # Test for process_audio_chunks
+    @patch("machine_learning_client.ml.sf.read")
+    @patch("machine_learning_client.ml.crepe.predict")
+    @patch("machine_learning_client.ml.pretty_midi.hz_to_note_number")
+    @patch("machine_learning_client.ml.pretty_midi.note_number_to_name")
+    def test_process_audio_chunks(
+        self,
+        mock_note_number_to_name,
+        mock_hz_to_note_number,
+        mock_crepe_predict,
+        mock_soundfile_read,
+    ):
+        """Mock audio processing"""
+        fake_audio = np.random.rand(1024 * 20)  # Example fake audio data
+        sr = 22050  # Example sampling rate
+        mock_soundfile_read.return_value = (fake_audio, sr)
+
+        mock_crepe_predict.return_value = (
+            np.array([0.1, 0.2]),  # time
+            np.array([440, 880]),  # frequency
+            np.array([0.8, 0.75]),  # confidence
+            None,  # additional return value
+        )
+
+        mock_hz_to_note_number.side_effect = (
+            lambda f: 69 if f == 440 else 81
+        )  # A4 and A5 note numbers
+        mock_note_number_to_name.side_effect = lambda n: "A4" if n == 69 else "A5"
+
+        notes_data = ml.process_audio_chunks(fake_audio, sr)
+        assert len(notes_data) >= 2, "Should at least identify two notes"
+        assert notes_data[0]["note"] == "A4", "First note should be A4"
+        assert notes_data[1]["note"] == "A5", "Second note should be A5"
+        assert all(
+            n["confidence"] >= 0.74 for n in notes_data
+        ), "Confidence should be above threshold"
+
+    # Test for clean_up_files
+    @patch("machine_learning_client.ml.os.remove")
+    def test_clean_up_files(self, mock_remove):
+        """Mock remove temporary audio files."""
+        webm_file = "test_audio.webm"
+        wav_file = "test_audio.wav"
+
+        ml.clean_up_files(webm_file, wav_file)
+
+        mock_remove.assert_any_call(webm_file)
+        mock_remove.assert_any_call(wav_file)
+        assert mock_remove.call_count == 2, "os.remove should be called twice"
+
+    # Test for write_audio_to_file
+    def test_write_audio_to_file(self, mocker):
+        """Given file name and audio stream, write audio to file"""
+
+        # Create mock audio stream data
+        test_data = b"This is test audio data"
+        mocker.read.return_value = test_data
+
+        # Define a test file name
+        test_file_name = "test_audio_file.webm"
+
+        # Call the function with the mock stream
+        ml.write_audio_to_file(test_file_name, mocker)
+
+        # Read back the file and assert its contents
+        with open(test_file_name, "rb") as file:
+            file_content = file.read()
+            assert (
+                file_content == test_data
+            ), "The file content does not match the expected data"
+
+        # Verify that read method was called on the mock
+        mocker.read.assert_called_once()
+
+        # Cleanup: Remove the test file
+        if os.path.exists(test_file_name):
+            os.remove(test_file_name)
+
+    # Test for sort_notes_data
+    def test_sort_notes_data(self):
+        """Test function to sort notes data"""
+        notes_data = [
+            {"note": "C", "time": 1.0},
+            {"note": "A", "time": 0.5},
+            {"note": "B", "time": 0.75},
+        ]
+
+        expected_sorted_notes = [
+            {"note": "A", "time": 0.5},
+            {"note": "B", "time": 0.75},
+            {"note": "C", "time": 1.0},
+        ]
+
+        sorted_notes = ml.sort_notes_data(notes_data)
+
+        assert (
+            sorted_notes == expected_sorted_notes
+        ), "The sorted notes do not match the expected result"
+
+    # Test for process_notes
+    def test_process_notes(self):
+        """Test function to process notes"""
+        notes_data = [
+            {"note": "C", "time": 0.1},
+            {"note": "C", "time": 0.2},
+            {"note": "D", "time": 0.3},
+            {"note": "E", "time": 0.4},
+            {"note": "E", "time": 0.5},
+        ]
+
+        expected_processed_notes = [{"note": "C"}, {"note": "E"}]
+
+        processed_notes = ml.process_notes(notes_data)
+
+        assert (
+            processed_notes == expected_processed_notes
+        ), "The sorted notes do not match the expected result"
+
+    # Test for generate_midi_url
+    @patch("machine_learning_client.ml.create_midi")
+    def test_generate_midi_url(self, mock_create_midi):
+        """Test generating midi url"""
+        mock_create_midi.return_value = "output.mid"
+
+        filtered_combined_notes = ["C", "D", "E"]  # Example note data
+        onsets = [0.1, 0.5, 1.0]  # Example onset times
+        durations = [0.4, 0.5, 0.6]  # Example note durations
+        tempo = 120  # Example tempo
+
+        # Expected URL
+        expected_url = "http://localhost:5002/static/output.mid"
+
+        # Call the function
+        result_url = ml.generate_midi_url(
+            filtered_combined_notes, onsets, durations, tempo
+        )
+
+        # Assert that the URL is correctly formed
+        assert (
+            result_url == expected_url
+        ), "The generated MIDI URL does not match the expected result"
+
+        # Additionally, check that create_midi was called with the correct arguments
+        mock_create_midi.assert_called_once_with(
+            filtered_combined_notes, onsets, durations, tempo, output_file="output.mid"
+        )
+
+    # Test for app process route
+
+    # Test for smooth_pitch_data
+    def test_smooth_pitch_data(self):
+        """Test smoothing pitch data."""
+
+        notes_data = [
+            {"note": "C", "time": 0.1},
+            {"note": "C", "time": 0.2},
+            {"note": "D", "time": 0.3},
+            {"note": "E", "time": 0.4},
+            {"note": "E", "time": 0.5},
+        ]
+
+        expected_smoothed_data = [
+            {"time": 0.20000000000000004, "note": "C"},
+            {"time": 0.25, "note": "C"},
+            {"time": 0.3, "note": "C"},
+            {"time": 0.35, "note": "E"},
+            {"time": 0.39999999999999997, "note": "E"},
+        ]
+
+        # Call the function
+        result = ml.smooth_pitch_data(notes_data)
+
+        assert (
+            result == expected_smoothed_data
+        ), "The smoothed data does not match the expected result"
+
+    # Test for filter_and_combine_notes
+    def test_filter_and_combine_notes(self):
+        """Test function to filter and combine notes."""
+
+        test_notes_data = [
+            {"note": "C", "time": 0.1},
+            {"note": "C", "time": 0.2},
+            {"note": "D", "time": 0.3},
+            {"note": "E", "time": 0.4},
+            {"note": "E", "time": 0.5},
+        ]
+
+        expected_filtered_notes = [{"note": "C"}, {"note": "D"}, {"note": "E"}]
+
+        # Call the function
+        result = ml.filter_and_combine_notes(test_notes_data)
+
+        # Assertions to check if the function behaves as expected
+        assert (
+            result == expected_filtered_notes
+        ), "The filtered notes do not match the expected result"
+
+    # Test for detect_note_onsets
+    @patch("machine_learning_client.ml.librosa.onset.onset_detect")
+    @patch("machine_learning_client.ml.librosa.load")
+    def test_detect_note_onsets(self, mock_load, mock_onset_detect):
+        """Test to detect when notes begin or onset."""
+        mock_load.return_value = (
+            None,
+            44100,
+        )  # y (audio signal) can be None as it's not used directly
+        expected_onsets = [0.1, 0.5, 1.0]  # Example onsets
+        mock_onset_detect.return_value = expected_onsets
+
+        # Test data
+        audio_file = "test_audio.wav"
+
+        # Call the function
+        result_onsets = ml.detect_note_onsets(audio_file)
+
+        # Assertions to check if the function behaves as expected
+        assert (
+            result_onsets == expected_onsets
+        ), "The detected onsets do not match the expected result"
+
+        # Check if the mocked methods were called with correct arguments
+        mock_load.assert_called_once_with(audio_file, sr=44100)
+        mock_onset_detect.assert_called_once_with(y=None, sr=44100, units="time")
+
+    # Test for estimate_tempo
     def test_estimate_tempo(self):
         """
         Checks if estimated tempo calculated from test_audio is same as calculated value
